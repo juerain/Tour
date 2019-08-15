f7f0f95f77ab68492454804449a2bef3de3504ed
juerain <1655292116@qq.com>
2019-08-15 19:08:37 +0800

    update

--- 畅游/vue_app_000/vue_app_00/src/router.js
+++ 畅游/vue_app_000/vue_app_00/src/router.js
@@ -13,10 +13,10 @@ import Search from "./components/ch/common/First-home/Search.vue"//搜索用户�
 import AddTv1 from "./components/ch/common/First-home/AddTv-1.vue"//开启相册功能页面
 import Camera from "./components/ch/common/First-home/Camera.vue"//开启相机功能
 import SuggestListAllMsg from "./components/ch/common/First-home/SuggestListAllMsg.vue"//推荐网友列表主页面
-<<<<<<< HEAD
-=======
+
+// 淘
 import Follow from "./components/ch/common/Follow/Follow.vue"//关注页
->>>>>>> 3eab909a3a7a9e38db9c1c6404a7242feeeb4eaa
+
 
 // // 李
 // // 引入Home.vue
@@ -32,8 +32,7 @@ import regist from "./components/ch/common/second-search/Regist.vue"
 // 引入登录组件
 import login from "./components/ch/common/second-search/Login.vue"
 
-// // 陶
-import Follow from "./components/ch/common/Follow.vue"//关注页
+
 
 
 
